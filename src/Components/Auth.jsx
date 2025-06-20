c8ddc34d1ed01ccda9364b570d62ffe06f1524b1
BracketProto <creepertruck812@gmail.com>
2025-06-20 14:41:23 +0100

    Update Auth.jsx

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,29 +1,33 @@
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Auth() {
-  const CookiesList = await cookies();
-  const Host = CookiesList.get("host")?.value;
-  const Protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+  const HeadersList = headers();
+  const Host = HeadersList.get("host");
+  const Protocol =
+    process.env.NODE_ENV === "production"
+      ? HeadersList.get("x-forwarded-proto") || "https"
+      : "http";
   const BaseUrl = `${Protocol}://${Host}`;
 
   try {
     const UserRes = await fetch(`${BaseUrl}/api/auth/discord/user`, {
-      credentials: "include", // Ensures cookies are sent
+      credentials: "include",
       cache: "no-store",
     });
 
     if (!UserRes.ok) {
       console.error("API Error:", UserRes.status, UserRes.statusText);
       redirect("/api/auth/discord");
+      return;
     }
 
     const UserData = await UserRes.json();
     console.log("User Data:", UserData);
-
     return UserData;
   } catch (Error) {
     console.error("Fetch Error:", Error);
     redirect("/api/auth/discord");
+    return;
   }
 }
